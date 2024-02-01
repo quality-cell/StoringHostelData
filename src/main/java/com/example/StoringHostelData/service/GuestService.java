7b42afcae7a1a5c19e69d4609ddcbbed1a560909
quality-cell <malko.anton3@mail.ru>
2024-02-01 12:33:02 +0700

    add service

---
+++ src/main/java/com/example/StoringHostelData/service/GuestService.java
@@ -0,0 +1,97 @@
+package com.example.StoringHostelData.service;
+
+import com.example.StoringHostelData.dto.GuestDTO;
+import com.example.StoringHostelData.entity.Guest;;
+import com.example.StoringHostelData.repository.GuestRepository;
+import com.example.StoringHostelData.repository.RoomRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GuestService {
+    private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+
+    @Autowired
+    public GuestService(RoomRepository roomRepository, GuestRepository guestRepository) {
+        this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+
+    }
+
+    public List<GuestDTO> getAllGuest() {
+        List<GuestDTO> res = new ArrayList<>();
+        guestRepository.findAll().forEach(guest -> res.add(new GuestDTO(guest)));
+
+        if (res.isEmpty()) {
+            throw new EntityNotFoundException("Постояльцев не найдено");
+        }
+
+        return res;
+    }
+
+    public GuestDTO getGuestById(Integer guestId) {
+        Optional<Guest> guest = guestRepository.findById(guestId);
+
+        if (guest.isPresent()) {
+            return new GuestDTO(guest.get());
+        }
+
+        throw new EntityNotFoundException("Постояльца с id: " + guestId + " не найден");
+    }
+
+    public List<GuestDTO> getByRoomId(Integer id) {
+        List<GuestDTO> guestDTOList = guestRepository.findByRoomId(id).stream().map(GuestDTO::new).toList();
+
+        if (guestDTOList.isEmpty()) {
+            throw new EntityNotFoundException("Комната с id: " + id + " пуста");
+        }
+
+        return guestDTOList;
+    }
+
+    public void createGuest(Integer id, GuestDTO guestDTO) {
+        roomRepository.findById(id)
+                .ifPresent(room -> guestRepository
+                        .save(new Guest(room,
+                                guestDTO.getSecondName(),
+                                guestDTO.getName(),
+                                guestDTO.getSurname(),
+                                guestDTO.getGender(),
+                                LocalDate.now(),
+                                LocalDate.now())));
+    }
+
+    public void updateGuest(Integer id, GuestDTO guestDTO) {
+        Optional<Guest> guestOptional = guestRepository.findById(id);
+
+        if (guestOptional.isEmpty()) {
+            throw new EntityNotFoundException("Постояльца с id: " + id + " не найдено");
+        }
+
+        Guest guest = guestOptional.get();
+        guest.setGender(guestDTO.getGender());
+        guest.setSurname(guestDTO.getSurname());
+        guest.setSecondName(guestDTO.getSecondName());
+        guest.setName(guestDTO.getName());
+        guest.setDateOfChange(LocalDate.now());
+
+        guestRepository.save(guest);
+    }
+
+    public void deleteGuest(Integer guestId) {
+        Optional<Guest> guestOptional = guestRepository.findById(guestId);
+
+        if (guestOptional.isEmpty()) {
+            throw new EntityNotFoundException("Постояльца с id: " + guestId + " не найдено");
+        }
+
+        guestRepository.deleteById(guestId);
+    }
+}
---
+++ src/main/java/com/example/StoringHostelData/service/RoomService.java
@@ -0,0 +1,85 @@
+package com.example.StoringHostelData.service;
+
+import com.example.StoringHostelData.dto.RoomDTO;
+import com.example.StoringHostelData.entity.Guest;
+import com.example.StoringHostelData.entity.Room;
+import com.example.StoringHostelData.repository.GuestRepository;
+import com.example.StoringHostelData.repository.RoomRepository;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoomService {
+    private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+
+    @Autowired
+    public RoomService(RoomRepository roomRepository, GuestRepository guestRepository) {
+        this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+    }
+
+    public List<RoomDTO> getAllRooms() {
+        List<RoomDTO> res = new ArrayList<>();
+        roomRepository.findAll().forEach(room -> res.add(new RoomDTO(room)));
+
+        if (res.isEmpty()) {
+            throw new EntityNotFoundException("Комнат не найдено");
+        }
+
+        return res;
+    }
+
+    public RoomDTO getRoomById(Integer roomId) {
+        Optional<Room> room = roomRepository.findById(roomId);
+
+        if (room.isPresent()) {
+            return new RoomDTO(room.get());
+        }
+
+        throw new EntityNotFoundException("Комнаты с id: " + roomId + " не найдено");
+    }
+
+    public void addRoom(Room room) {
+        roomRepository.save(room);
+    }
+
+    public void updateRoom(Integer id, RoomDTO roomDTO) {
+        Optional<Room> roomOptional = roomRepository.findById(id);
+        List<Guest> guestOptional = guestRepository.findByRoomId(id);
+
+        if (roomOptional.isEmpty()) {
+            throw new EntityNotFoundException("Комнаты с id: " + id + " не найдено");
+        }
+
+        if (!guestOptional.isEmpty()) {
+            throw new EntityExistsException("Комнаты с id: " + id + " ещё занята и ее нельзя обновить");
+        }
+
+        Room room = roomOptional.get();
+        room.setRoomType(roomDTO.getRoomType());
+        room.setComfortType(roomDTO.getComfortType());
+        room.setRoomNumber(roomDTO.getRoomNumber());
+        room.setNumberOfSeats(roomDTO.getNumberOfSeats());
+        room.setDateOfChange(LocalDate.now());
+
+        roomRepository.save(room);
+    }
+
+    public void deleteRoom(Integer roomId) {
+        Optional<Room> roomOptional = roomRepository.findById(roomId);
+
+        if (roomOptional.isEmpty()) {
+            throw new EntityNotFoundException("Комнаты с id: " + roomId + " не найдено");
+        }
+
+        roomRepository.deleteById(roomId);
+    }
+}
