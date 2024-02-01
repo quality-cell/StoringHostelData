22f2791d49180cdae9c949a6cd73548ed88f133d
quality-cell <malko.anton3@mail.ru>
2024-02-01 12:26:39 +0700

    init

---
+++ src/main/java/com/example/StoringHostelData/StoringHostelData.java
@@ -0,0 +1,13 @@
+package com.example.StoringHostelData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoringHostelData {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoringHostelData.class, args);
+	}
+
+}
